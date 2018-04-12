7166b14b69c4a1d2725c03dd35e80d69056e1b46
Arda I <ai67@duke.edu>
2018-04-12 12:37:36 -0400

    minor changes to comments

--- src/controller/ControllerServlet.java
+++ src/controller/ControllerServlet.java
@@ -30,17 +30,12 @@ public class ControllerServlet extends HttpServlet {
 		
 		try{
             out.println("Connected to database!");
-            out.println(Printer.getSalt("arda@gmail.com"));
-            out.println(Printer.authenticate("arda@gmail.com", "Arda's hash"));
         }catch(Exception e){
             e.printStackTrace();
         }
         
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
